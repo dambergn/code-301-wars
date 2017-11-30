f943e4294b21e5139c11c01ee8c4da3b19d518d0
dambergn <dambergn@gmail.com>
2017-11-29 19:52:41 -0800

    re-structured file to suit assignment requirements.

---
+++ replace-all-dots/solution.js
@@ -0,0 +1,8 @@
+'use strict';
+
+//note I was not able to get this to work outside of code wars, if you run the html version you can inspect it and see it work in console.
+
+function replaceDots(str) {
+  var rep = str.replace(/\./g, '-');
+  return rep;
+};
